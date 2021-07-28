453dedb4de0ed0a47b81bb2a844173e8f8a4ecf2
denis <denis@kustov.com>
2021-07-28 22:49:08 +0300

    Работает переключение языков

--- app/logic.py
+++ app/logic.py
@@ -26,23 +26,7 @@ class LogicCapture:
     TriggerCopy: bool = False
 
     is_FlagLiveThread: bool = True
-    # switchKey = {
-    #     # Отчистка массива при смене фокуса
-    #     "Key.enter": lambda: LogicCapture.KeyLettersAlphabet.clear(),
-    #     "Key.backspace": lambda: LogicCapture.KeyLettersAlphabet.clear(),
-    #     "Key.delete": lambda: LogicCapture.KeyLettersAlphabet.clear(),
-    #     "Key.up": lambda: LogicCapture.KeyLettersAlphabet.clear(),
-    #     "Key.down": lambda: LogicCapture.KeyLettersAlphabet.clear(),
-    #     "Key.left": lambda: LogicCapture.KeyLettersAlphabet.clear(),
-    #     "Key.right": lambda: LogicCapture.KeyLettersAlphabet.clear(),
-    #     "Key.shift": lambda: LogicCapture._ChangeLang(),
-    #     # При вызове перевода
-    #     "Key.alt_l": lambda: LogicCapture._PressDualAlt(),
-    #     # добавить пробелы
-    #     "Key.space": lambda: LogicCapture.KeyLettersAlphabet.append(0),
-    #     "Key.ctrl_r": lambda: printDebug(f"[HASH]\t{LogicCapture._ConversionListCodeKeyToSir()}"),
-    #     "Key.ctrl_l": lambda: LogicCapture._CopyTrigger()
-    # }
+    CopyBuffer: str = ""
 
     KeyboardOdj = KeyboardHunter()
     MouseObj = MouseHunter()
@@ -75,27 +59,33 @@ class LogicCapture:
         return res_text
 
     @classmethod
-    def _SetTranslateText(cls, text: str, delFlag=True):
-        printDebug(f"[INPUT]\t{text}")
-        # перевод раскладки
-        NewTranslateKey: List[str] = cls._SibelTranslation(text)
-        ln = len(NewTranslateKey)
-        printDebug(f"[ LEN]\t{ln}")
-        printDebug(f"[ RES]\t{NewTranslateKey}")
+    def _SetTranslateText(cls, text: str):
 
-        if not delFlag:
-            cls.KeyboardOdj.KeyBoard.tap(keyboard.Key.right)
-            cls.SkipTrigger = (ln * 4) + 1
-        else:
-            cls.SkipTrigger = ln * 4
-
-        # Стереть слова на неправильной раскладки
-        for _ in range(ln):
+        if cls.CopyBuffer:
+            printDebug(f"[INPUT COPY]\t{cls.CopyBuffer}")
+            # перевод раскладки
+            NewTranslateKey: List[str] = cls._SibelTranslation(cls.CopyBuffer)
+            ln = len(NewTranslateKey)
+            printDebug(f"[ LEN]\t{ln}")
+            printDebug(f"[ RES]\t{NewTranslateKey}")
+            # Стереть слова на неправильной раскладки
             kb.send("backspace")
 
-        kb.write(NewTranslateKey, delay=0.05)
 
+        else:
+            printDebug(f"[INPUT]\t{text}")
+            # перевод раскладки
+            NewTranslateKey: List[str] = cls._SibelTranslation(text)
+            ln = len(NewTranslateKey)
+            printDebug(f"[ LEN]\t{ln}")
+            printDebug(f"[ RES]\t{NewTranslateKey}")
+            # Стереть слова на неправильной раскладки
+            for _ in range(ln):
+                kb.send("backspace")
+
+        kb.write(NewTranslateKey, delay=0.05)
         cls.KeyLettersAlphabet.clear()
+        cls.CopyBuffer = ""
 
     # @classmethod
     # def _PressDualAlt(cls):
@@ -118,9 +108,10 @@ class LogicCapture:
     # def _ChangeLang(cls):
     #     cls.TriggerChangeLang = True
     #
-    # @classmethod
-    # def _CopyTrigger(cls):
-    #     cls.TriggerCopy = True
+    @classmethod
+    def _CopyTrigger(cls):
+        cls.CopyBuffer = paste()
+        printDebug("[COPY]")
 
     @classmethod
     def PressMouse(cls, *args):
@@ -156,14 +147,14 @@ class LogicCapture:
                              80: "^",
                              28: "enter",
                              1: "esc",
-                             61: "f3"
+                             61: "f3",
                              }
         HotKey: List[str] = []
 
         ArrHotKey: Dict = {
-            37.5: lambda: printDebug("[COPY]"),
-            61.0: lambda: printDebug(f"{LogicCapture._ConversionListCodeKeyToSir()}"),
-            30.0: lambda: LogicCapture._SetTranslateText(LogicCapture._ConversionListCodeKeyToSir())
+            (29, 46): lambda: LogicCapture._CopyTrigger(),
+            (61,): lambda: printDebug(f"{LogicCapture._ConversionListCodeKeyToSir()}"),
+            (56, 44): lambda: LogicCapture._SetTranslateText(LogicCapture._ConversionListCodeKeyToSir())
         }
 
         def pressed_keys(event):
@@ -179,6 +170,11 @@ class LogicCapture:
             et = 1 if event.event_type == 'down' else 0
 
             if SpecialSelectKey[1] != sc:
+
+                if sc in (541,):
+                    # 541 alt up вредная клавиша неизвестного происхождения
+                    return None
+
                 if ArrKeySpecialKeys.get(sc, False):
                     if et and SpecialSelectKey[0] == False:
                         SpecialSelectKey[0] = True
@@ -186,6 +182,7 @@ class LogicCapture:
                         HotKey.clear()
                         HotKey.append(sc)
                         printDebug(f"[SelectKey]\t{SpecialSelectKey}")
+                        return None
 
                     elif et:
                         HotKey.append(sc)
@@ -203,10 +200,8 @@ class LogicCapture:
             elif not et:
                 SpecialSelectKey[0] = False
                 SpecialSelectKey[1] = -1
-                mean = sum(HotKey) / len(HotKey)
                 printDebug(f"[SelectKey]\t{SpecialSelectKey}")
                 printDebug(f"[HotKey]\t{HotKey}")
-                printDebug(f"[Mean HotKey]\t{mean}")
                 printDebug(f"[LKA 2]\t{LogicCapture.KeyLettersAlphabet}")
 
                 # Верификация множественного нажатия на alt + shift
@@ -214,7 +209,6 @@ class LogicCapture:
                     if len(HotKey[1:]) % 2 != 0:
                         LogicCapture.MainLangKeyboard = 'en' if LogicCapture.MainLangKeyboard == 'ru' else 'ru'
                     printDebug(f"[MainLangKeyboard]\t{LogicCapture.MainLangKeyboard}")
-                    # LogicCapture._SetTranslateText(LogicCapture._ConversionListCodeKeyToSir())
 
                 # Если печатали с зажатым shift
                 elif HotKey[0] == 42 and len(HotKey) > 1:
@@ -224,8 +218,8 @@ class LogicCapture:
                             LogicCapture.KeyLettersAlphabet.append(key_iter * -1)
 
                 else:
-                    if ArrHotKey.get(mean, False):
-                        ArrHotKey[mean]()
+                    if ArrHotKey.get(tuple(HotKey), False):
+                        ArrHotKey[tuple(HotKey)]()
 
                 return None
 
@@ -234,106 +228,12 @@ class LogicCapture:
                 printDebug(f"[KeyClick]\t{sc}")
                 printDebug(f"[LKA 1]\t{LogicCapture.KeyLettersAlphabet}")
 
-            # printDebug(f"{event.name} - {sc}")
-            #
-            # nonlocal ShiftTrigger, TriggerChangeLang, TriggerCopy
-            #
-            # # Принудительно зактрыть поток
-            # if not cls.is_FlagLiveThread:
-            #     keyboard.unhook_all()
-            #     sys.exit(-1)
-            #
-            # # Клавиши при нажатие которые отчищают массив
-            # if event.scan_code in (14, 28, 1, 83, 75, 72, 80, 77):  # Enter; Backspace; Esc; Delete; Стрелки перемещения
-            #     # Отчиска массива при смене фокуса
-            #     LogicCapture.PressedKeys.clear()
-            #     return None
-            #
-            # # Обработка печататния с зажатым shift
-            # if event.name == "shift" and event.event_type == 'down':  # обработать зжатия Shift -
-            #     ShiftTrigger = 1000
-            # elif event.name == "shift" and event.event_type == 'up':
-            #     ShiftTrigger = 0
-            #
-            # # Обработка копирования текста
-            # if event.name == "ctrl":
-            #     if event.event_type == "down":
-            #         TriggerCopy = 1
-            #     else:
-            #         return None
-            # if event.scan_code == 46:  # "c"
-            #     if TriggerCopy == 1 and event.event_type == "up":
-            #         TriggerCopy = 2
-            #         return None
-            #
-            # # Обработка тригера комбинации перевода текста с задержко по времни нажатия в 1 секунуд
-            # if event.name == 'alt' and event.event_type == 'up':
-            #     ShiftTrigger = 0
-            #     if TriggerChangeLang == int(time.time()):
-            #         if TriggerCopy == 2:
-            #             LogicCapture.SetTranslateText(pyperclip.paste(), delFlag=False)
-            #         else:
-            #             LogicCapture.SetTranslateText(LogicCapture.CodeKeyTranslation(LogicCapture.PressedKeys),
-            #                                           delFlag=True)
-            #     else:
-            #         TriggerChangeLang = int(time.time())
-            #     return None
-            #
-            # else:
-            #     # Запись нажатых клавишь с учетом печатанью через Shift
-            #     if event.event_type == "up":
-            #         LogicCapture.PressedKeys.append(event.scan_code + ShiftTrigger)
-            #         TriggerCopy = 0
-            #
-            # log.debug(LogicCapture.CodeKeyTranslation(LogicCapture.PressedKeys))  # DEBUG
-            #
-
         # Отчистка буфера обмена
         if windll.user32.OpenClipboard(None):
             windll.user32.EmptyClipboard()
             windll.user32.CloseClipboard()
         kb.hook(pressed_keys)
         kb.wait()
-
-        # if cls.SkipTrigger == 0:
-        #
-        #     if hasattr(key, 'char'):
-        #         printDebug(f"164: {cls.PressedKeys}", key="Обработка нажатий")
-        #
-        #         if cls.TriggerCopy and key.vk == 67:
-        #             printDebug(f"[COPY]\t{paste()}", key="Обработка нажатий")
-        #
-        #         try:
-        #             if key.vk:
-        #                 if key.char.istitle():
-        #                     cls.PressedKeys.append(key.vk * -1)
-        #
-        #                 else:
-        #                     cls.PressedKeys.append(key.vk)
-        #
-        #         except (AttributeError, TypeError) as e:
-        #             printDebug(f"179: {e}", key="Обработка нажатий; Ошибки")
-        #
-        #     else:
-        #         try:
-        #             # Слежка за сменой раскладки клавиатуры
-        #             if cls.TriggerChangeLang and str(key) == "Key.alt_l":
-        #                 cls.MainLangKeyboard = 'en' if cls.MainLangKeyboard == 'ru' else 'ru'
-        #                 printDebug(f"185: {cls.MainLangKeyboard} = {fileImage[LogicCapture.MainLangKeyboard]}",
-        #                               key="Обработка нажатий; Смена языка")
-        #             else:
-        #                 cls.TriggerChangeLang = False
-        #
-        #             if str(key) != "Key.alt_l":
-        #                 cls.TriggerCopy = False
-        #
-        #             cls.switchKey[str(key)]()
-        #             printDebug(f"190: {key}", key="Обработка нажатий")
-        #         except KeyError as e:
-        #             printDebug(f"193: {e}", key="Обработка нажатий; Ошибки")
-        #
-        # else:
-        #     cls.SkipTrigger -= 1
 
 
 if __name__ == '__main__':
